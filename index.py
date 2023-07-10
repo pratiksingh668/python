5f11358b231830f6598744b62655669d66f0227c
pratiksingh668 <121291511+pratiksingh668@users.noreply.github.com>
2023-07-10 14:51:20 +0530

    index.py

--- index.py
+++ index.py
@@ -1,10 +1,6 @@
 from http.server import BaseHTTPRequestHandler
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+a=int(input("enter no =  "))
+if a>1:
+    print("Even")
+else:
+    print("odd")
